a2a51173971741d6d1b3018c9cfe7998e3c580dc
Pablo Ortega <pablo.ortega13@yahoo.fr>
2020-02-06 11:49:50 +0100

    Fin des methodes speciales

--- analysis/csv.py
+++ analysis/csv.py
@@ -15,6 +15,9 @@ class SetOfParliamentMembers:
     def __init__(self, name):
         self.name = name
 
+    def __repr__(self):
+    	return "SetOfParliamentMembers total : {}".format(len(self.dataframe))
+
     def data_from_csv(self, csv_file):
         self.dataframe = pd.read_csv(csv_file, sep=";")
 
@@ -52,22 +55,30 @@ class SetOfParliamentMembers:
 	        all_parties = data["parti_ratt_financier"].dropna().unique()
 	        for party in all_parties:
 	        	data_subset = data[data.parti_ratt_financier == party]
-	        	subset = SetOfParliamentMember('MPs from party "{}"'.format(party))
+	        	subset = SetOfParliamentMembers('MPs from party "{}"'.format(party))
 	        	subset.data_from_dataframe(data_subset)
 	        	result[party] = subset
 
 	        	return result
 
 
-def launch_analysis(data_file, by_party = False):
+def launch_analysis(data_file, by_party = False, info = False):
 	sopm = SetOfParliamentMembers("All MPs")
 	sopm.data_from_csv(os.path.join("data",data_file))
-	sopm.display_chart()
+	#sopm.display_chart()
 
 	if by_party:
 		for party, s in sopm.split_by_political_party().items():
-			s.display_chart()
+			lg.debug("by party")
+			s.display_chart() 
+
+	elif info:
+		print(sopm)
+
+	else:
+		sopm.display_chart()
 	
+    
 
 if __name__ == '__main__':
 	launch_analysis(data_file)
--- partite.py
+++ partite.py
@@ -5,24 +5,36 @@ import argparse
 
 import analysis.csv as c_an
 import analysis.xml as x_an 
-
+import logging as lg
 
 def parse_argument():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-e","--extension",help="""XML or CSV ?""")
 	parser.add_argument("-d","--datafile",help="""You have to load a datafile""")
-	parser.add_argument("-i","--info",help="""Information about the file""")
+	parser.add_argument("-p","--byparty",action='store_true', help="""Do you want to split by political party ?""")
+	parser.add_argument("-i","--info",action='store_true', help="""Information about the file""")
 	return parser.parse_args()
 	pass
 
 
 def main():
 	args = parse_argument()
-	if args.extension == 'csv':
-		c_an.launch_analysis(args.datafile)
-	elif args.extension == 'xml':
-		x_an.launch_analysis(args.datafile)
+	try:
+		datafile = args.datafile
+		if datafile == None:
+			raise Warning("You must indicate a datafile")
+
+	except Warning as e:
+		lg.warning(e)
 	
+	else:
+		if args.extension == 'csv':
+			c_an.launch_analysis(args.datafile, args.byparty, args.info)
+		elif args.extension == 'xml':
+			x_an.launch_analysis(args.datafile, args.party, args.info)
+	
+	finally:
+		lg.info("Analysis if over!!")
 
 if __name__ == '__main__':
 	main()
